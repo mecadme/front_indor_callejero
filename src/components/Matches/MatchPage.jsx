2e3b8211194f5bf63e23b0d3f11c2e1282e7ea71
Mauro Cadme <mecadme@hotmail.com>
2024-10-08 21:18:17 -0500

    improve matches

--- src/components/Matches/MatchPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MatchPage = () => {
-  return (
-    <div>MatchPage</div>
-  )
-}
-
-export default MatchPage
--- src/components/Matches/Matches.jsx
+++ src/components/Matches/Matches.jsx
@@ -1,18 +1,77 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import useFetchMatches from '../../hooks/useFetchMatches';
-import { Card, Row, Col, Container } from 'react-bootstrap';
-import EmptyData from '../Administration/EmptyData';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Container, Row, Form, Pagination } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useGetMatches } from "../../api/Service/MatchService";
+import EmptyData from "../Administration/EmptyData";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Loading from "../Utils/Loading";
 
 const Matches = () => {
-  const { matches, isLoading, error } = useFetchMatches();
+  const {
+    data: matches = [],
+    loading: isLoading,
+    error,
+    getMatches,
+  } = useGetMatches(); // matches inicializado como array vacío
+  const [filteredMatches, setFilteredMatches] = useState([]);
+  const [searchDate, setSearchDate] = useState("");
+  const [teamFilter, setTeamFilter] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const matchesPerPage = 4;
+
   const navigate = useNavigate();
 
+  useEffect(() => {
+    getMatches();
+  }, []);
+
+  useEffect(() => {
+    if (Array.isArray(matches)) {
+      const sortedMatches = [...matches].sort(
+        (a, b) => new Date(b.schedule.date) - new Date(a.schedule.date)
+      );
+      setFilteredMatches(sortedMatches);
+    }
+  }, [matches]);
+
+  useEffect(() => {
+    if (Array.isArray(matches)) {
+      let filtered = [...matches];
+      if (searchDate) {
+        filtered = filtered.filter(
+          (match) =>
+            new Date(match.schedule.date).toLocaleDateString() ===
+            new Date(searchDate).toLocaleDateString()
+        );
+      }
+      if (teamFilter) {
+        filtered = filtered.filter(
+          (match) =>
+            match.homeTeam.name.includes(teamFilter) ||
+            match.awayTeam.name.includes(teamFilter)
+        );
+      }
+      setFilteredMatches(filtered);
+    }
+  }, [searchDate, teamFilter, matches]);
+
+  const handleMatchClick = (matchId) => {
+    navigate(`/result/${matchId}`);
+  };
+
+  const indexOfLastMatch = currentPage * matchesPerPage;
+  const indexOfFirstMatch = indexOfLastMatch - matchesPerPage;
+  const currentMatches = filteredMatches.slice(
+    indexOfFirstMatch,
+    indexOfLastMatch
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   if (isLoading) {
-    return <div>Cargando partidos...</div>;
+    return <Loading />;
   }
 
   if (error) {
@@ -20,109 +79,161 @@ const Matches = () => {
   }
 
   if (!matches || matches.length === 0) {
-    return (<Container fluid>
-      <Header />
-      <EmptyData />
-      <Footer />
-    </Container>)
+    return (
+      <Container fluid>
+        <Header />
+        <EmptyData />
+        <Footer />
+      </Container>
+    );
   }
-
-  const handleMatchClick = (matchId, homeTeam, awayTeam) => {
-    navigate(`/result/${matchId}`);
-  };
 
   return (
     <Container fluid>
       <Header />
-    <Container className="container mt-5">
-      <h2 className="text-center mb-4">Partidos</h2>
-      <Row>
-        {matches.map((match) => (
-          <Col key={match.matchId} md={6} className="mb-4"
-          onClick={() => handleMatchClick(match.matchId)}
-          style={{ cursor: "pointer",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
+      <Container className="container mt-5">
+        <h2 className="text-center mb-4">Partidos</h2>
 
-           }}
-          >
-            <Card>
-              <Card.Header className="d-flex justify-content-between align-items-center">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={match.homeTeam.logoUrl}
-                    alt={match.homeTeam.name}
-                    width="50"
-                    height="50"
-                    className="mr-2"
-                  />
-                  <h5 style={{ color: match.homeTeam.color }}>{match.homeTeam.name}</h5>
-                </div>
-                <span>VS</span>
-                <div className="d-flex align-items-center">
-                  <h5 style={{ color: match.awayTeam.color }}>{match.awayTeam.name}</h5>
-                  <img
-                    src={match.awayTeam.logoUrl}
-                    alt={match.awayTeam.name}
-                    width="50"
-                    height="50"
-                    className="ml-2"
-                  />
-                </div>
-              </Card.Header>
-              <Card.Body>
-                <p><strong>Fecha:</strong> {new Date(match.schedule.date).toLocaleDateString()}</p>
-                <p><strong>Lugar:</strong> {match.schedule.place}</p>
-                <Row>
-                  <Col md={6}>
-                    <h6>Jugadores del {match.homeTeam.name}</h6>
-                    <ul className="list-unstyled">
-                      {match.homeTeam.players.map((player) => (
-                        <li key={player.playerId} className="d-flex align-items-center my-2">
-                          <img
-                            src={player.photoUrl}
-                            alt={player.firstName}
-                            width="30"
-                            height="30"
-                            className="mr-2"
-                          />
-                          {player.firstName} {player.lastName}
-                        </li>
-                      ))}
-                    </ul>
-                  </Col>
-                  <Col md={6}>
-                    <h6>Jugadores del {match.awayTeam.name}</h6>
-                    <ul className="list-unstyled">
-                      {match.awayTeam.players.map((player) => (
-                        <li key={player.playerId} className="d-flex align-items-center my-2">
-                          <img
-                            src={player.photoUrl}
-                            alt={player.firstName}
-                            width="30"
-                            height="30"
-                            className="mr-2"
-                          />
-                          {player.firstName} {player.lastName}
-                        </li>
-                      ))}
-                    </ul>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer className="text-muted text-center">
-                <p><strong>Duración:</strong> {match.duration} hrs</p>
-                <p><strong>Fase:</strong> {match.phase}</p>
-                <p><strong>Estado:</strong> {match.status}</p>
-              </Card.Footer>
-            </Card>
+        <Row className="mb-4">
+          
+          <Col md={4}>
+            <Form.Group controlId="filterByTeam">
+              <Form.Label>Filtrar por equipo</Form.Label>
+              <Form.Control
+                as="select"
+                value={teamFilter}
+                onChange={(e) => setTeamFilter(e.target.value)}
+              >
+                <option value="">Todos los equipos</option>
+                {matches &&
+                  [
+                    ...new Set(
+                      matches.flatMap((match) => [
+                        match.homeTeam.name,
+                        match.awayTeam.name,
+                      ])
+                    ),
+                  ].map((team) => (
+                    <option key={team} value={team}>
+                      {team}
+                    </option>
+                  ))}
+              </Form.Control>
+            </Form.Group>
           </Col>
-        ))}
-      </Row>
-    </Container>
-    <Footer />
+        </Row>
+
+        <Row>
+        {currentMatches.map((match) => (
+            <Col
+              key={match.matchId}
+              md={6}
+              className="mb-4"
+              onClick={() => handleMatchClick(match.matchId)}
+              style={{
+                cursor: "pointer",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Card>
+                <Card.Header className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center">
+                    <img
+                      src={match.homeTeam.logoUrl}
+                      alt={match.homeTeam.name}
+                      width="50"
+                      height="50"
+                      className="mr-2"
+                    />
+                    <h5 style={{ color: match.homeTeam.color }}>
+                      {match.homeTeam.name}
+                    </h5>
+                  </div>
+                  <span>VS</span>
+                  <div className="d-flex align-items-center">
+                    <h5 style={{ color: match.awayTeam.color }}>
+                      {match.awayTeam.name}
+                    </h5>
+                    <img
+                      src={match.awayTeam.logoUrl}
+                      alt={match.awayTeam.name}
+                      width="50"
+                      height="50"
+                      className="ml-2"
+                    />
+                  </div>
+                </Card.Header>
+                <Card.Body>
+                  <p>
+                    <strong>Fecha:</strong>{" "}
+                    {new Date(match.schedule.date).toLocaleDateString()}
+                  </p>
+                  <p>
+                    <strong>Lugar:</strong> {match.schedule.place}
+                  </p>
+                  <Row>
+                    <Col md={6}>
+                      <h6>Jugadores del {match.homeTeam.name}</h6>
+                      <ul className="list-unstyled">
+                        {match.homeTeam.players.map((player) => (
+                          <li
+                            key={player.playerId}
+                            className="d-flex align-items-center my-2"
+                          >
+                            <img
+                              src={player.photoUrl}
+                              alt={player.firstName}
+                              width="30"
+                              height="30"
+                              className="mr-2"
+                            />
+                            {player.firstName} {player.lastName}
+                          </li>
+                        ))}
+                      </ul>
+                    </Col>
+                    <Col md={6}>
+                      <h6>Jugadores del {match.awayTeam.name}</h6>
+                      <ul className="list-unstyled">
+                        {match.awayTeam.players.map((player) => (
+                          <li
+                            key={player.playerId}
+                            className="d-flex align-items-center my-2"
+                          >
+                            <img
+                              src={player.photoUrl}
+                              alt={player.firstName}
+                              width="30"
+                              height="30"
+                              className="mr-2"
+                            />
+                            {player.firstName} {player.lastName}
+                          </li>
+                        ))}
+                      </ul>
+                    </Col>
+                  </Row>
+                </Card.Body>
+                <Card.Footer className="text-muted text-center">
+                  <p>
+                    <strong>Duración:</strong> {match.duration} hrs
+                  </p>
+                  <p>
+                    <strong>Fase:</strong> {match.phase}
+                  </p>
+                  <p>
+                    <strong>Estado:</strong> {match.status}
+                  </p>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <Footer />
     </Container>
   );
 };
